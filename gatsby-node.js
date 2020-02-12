c63d0bda141ceb2bc48e9445202e589d5b64e5a2
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2020-02-12 13:06:30 +0100

    Fixed gatsby-node exports

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,13 @@
 const { createResolvers } = require('./src/createResolvers');
 const { createSchemaCustomization } = require('./src/createSchemaCustomization');
 const { onCreateNode } = require('./src/onCreateNode');
-const { onPreBootstrap } = require('./src/onPreBootstrap');
+const { onPreInit } = require('./src/onPreInit');
 const { onPostBootstrap } = require('./src/onPostBootstrap');
 const { sourceNodes } = require('./src/sourceNodes');
 
-exports = {
-  createResolvers,
-  createSchemaCustomization,
-  onCreateNode,
-  onPreBootstrap,
-  onPostBootstrap,
-  sourceNodes,
-};
+exports.createResolvers = createResolvers;
+exports.createSchemaCustomization = createSchemaCustomization;
+exports.onCreateNode = onCreateNode;
+exports.onPreInit = onPreInit;
+exports.onPostBootstrap = onPostBootstrap;
+exports.sourceNodes = sourceNodes;
