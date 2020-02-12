f30500b3c8dc3bd72f423f3954e6f6c76fc2b560
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2020-02-12 13:07:00 +0100

    Renamed onPreBootstrap to onPreInit

--- src/onPreInit.ts
+++ src/onPreInit.ts
@@ -1,11 +1,15 @@
 import { ParentSpanPluginArgs } from 'gatsby';
 import { defaultOptions, SourceSyliusPluginOptions } from './options/SourceSyliusPluginOptions';
 
-export function onPreBootstrap(
+export function onPreInit(
   { reporter }: ParentSpanPluginArgs,
   options: SourceSyliusPluginOptions = defaultOptions,
 ):void {
   if (options.debug) {
     reporter.info('[Sylius Source] onPreBootstrap');
+  }
+
+  if (!options.url) {
+    reporter.panic('[Sylius Source] `url` option not provided!');
   }
 }
