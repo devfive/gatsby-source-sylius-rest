464bced32c3d6aaeb30a50cfc0314dd2e168d26e
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2020-02-13 19:20:40 +0100

    Added LatestProductsProvider

---
+++ src/data/providers/LatestProductsProvider.ts
@@ -0,0 +1,28 @@
+import { IRequestOptions } from 'typed-rest-client';
+import { SyliusProduct } from '../../schemas/Sylius/Product';
+import { RestDataProvider, RestDataProviderOptions } from './RestDataProvider';
+
+export type LatestProductsProviderOptions = Omit<RestDataProviderOptions, 'resourceName'>;
+
+export class LatestProductsProvider extends RestDataProvider<SyliusProduct> {
+  constructor(options: LatestProductsProviderOptions) {
+    super({
+      ...options,
+      resourceName: 'product-latest',
+    });
+  }
+
+  public async getRecords(options?: IRequestOptions):Promise<SyliusProduct[] | null> {
+    const requestOptions: IRequestOptions = {
+      ...options,
+      responseProcessor: this.processResponse.bind(this),
+    };
+
+    return super.getRecords(requestOptions);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private processResponse(response: { items: SyliusProduct[] }): SyliusProduct[] {
+    return response.items;
+  }
+}
