d6f40b75f7d05ebd549b2f32e42d21fbf5fa75c6
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2020-03-29 14:49:01 +0200

    Added report utility functions

---
+++ src/utils/reportDebug.ts
@@ -0,0 +1,31 @@
+import { Reporter } from 'gatsby';
+import { SyliusSourcePluginOptions } from '../schemas/Plugin';
+
+export type ReportType =
+  | 'panic'
+  | 'error'
+  | 'success'
+  | 'info'
+  | 'warn'
+  | 'log';
+
+export function reportDebug(
+  reporter: Reporter,
+  options: SyliusSourcePluginOptions,
+  message: string,
+  type: ReportType = 'info',
+): void {
+  if (!options.debug) {
+    return;
+  }
+
+  report(reporter, message, type);
+}
+
+export function report(
+  reporter: Reporter,
+  message: string,
+  type: ReportType = 'info',
+): void {
+  reporter[type](message);
+}
